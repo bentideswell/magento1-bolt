496bf54dbfa4328fbe78dcf2866e1f5681a7340d
Ben Tideswell <ben@fishpig.co.uk>
2018-03-13 07:16:07 +0000

    2.2.1.11

--- shell/bolt-flush.php
+++ shell/bolt-flush.php
@@ -1,38 +1,38 @@
 <?php
 /*
- *
+ * @SkipObfuscation
  */
-	ini_set('display_errors', 1);
-	error_reporting(E_ALL);
-	set_time_limit(0);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+set_time_limit(0);
 
-	// This is required incase extension is installed using modman
-	// Would usually use dirname(__DIR__) however is this file in a symlink
-	// This will point to the parent directory of the actual file and not the symlink
-	$dirsToTry = array(
-		getcwd(), 					// Calling from the Magento directory
-		dirname(getcwd()), 	// Calling from the shell directory
-		__DIR__, 						// Calling from the Magento directory
-		dirname(__DIR__), 	// Calling from the shell directory
-	);
+// This is required incase extension is installed using modman
+// Would usually use dirname(__DIR__) however is this file in a symlink
+// This will point to the parent directory of the actual file and not the symlink
+$dirsToTry = array(
+	__DIR__, 						// Calling from the Magento directory
+	dirname(__DIR__), 	// Calling from the shell directory
+	getcwd(), 					// Calling from the Magento directory
+	dirname(getcwd()), 	// Calling from the shell directory
+);
 
-	$ds = DIRECTORY_SEPARATOR;
+$ds = DIRECTORY_SEPARATOR;
+
+foreach($dirsToTry as $dir) {
+	$appMageFile = $dir . $ds . 'app' . $ds . 'Mage.php';
 	
-	foreach($dirsToTry as $dir) {
-		$appMageFile = $dir . $ds . 'app' . $ds . 'Mage.php';
-		
-		if (is_file($appMageFile)) {
-			chdir($dir);
-			include($appMageFile);
-			umask(0);
-			Mage::app();
-			break;
-		}
+	if (is_file($appMageFile)) {
+		chdir($dir);
+		include($appMageFile);
+		umask(0);
+		Mage::app();
+		break;
 	}
-	
-	if (!class_exists('Mage')) {
-		echo 'Unable to find Magento installation.';
-		exit;
-	}
+}
 
-	Mage::helper('bolt/cache_queue')->flush();
+if (!class_exists('Mage')) {
+	echo 'Unable to find Magento installation.';
+	exit;
+}
+
+Mage::helper('bolt/cache_queue')->flush();
