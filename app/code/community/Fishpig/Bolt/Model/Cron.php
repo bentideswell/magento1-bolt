5a0e97b35eca086a0ca76929bbcb69fd019110b2
Ben Tideswell <ben@fishpig.co.uk>
2018-04-05 15:43:10 +0100

    2.2.1.16

--- app/code/community/Fishpig/Bolt/Model/Cron.php
+++ app/code/community/Fishpig/Bolt/Model/Cron.php
@@ -9,6 +9,8 @@ class Fishpig_Bolt_Model_Cron
 	 */
 	public function flushCacheQueue()
 	{
-		return Mage::helper('bolt/cache_queue')->flush();
+		if (Mage::getStoreConfigFlag('bolt/autorefresh/cron_use_magento')) {
+			return Mage::helper('bolt/cache_queue')->flush();
+		}
 	}
 }
