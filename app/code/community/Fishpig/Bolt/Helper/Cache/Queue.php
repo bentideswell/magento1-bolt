3a7e69e4fadc25dd7bda7eff5db007db0cb91e78
Ben Tideswell <ben@fishpig.co.uk>
2018-09-11 10:02:36 +0100

    2.3.0.0

--- app/code/community/Fishpig/Bolt/Helper/Cache/Queue.php
+++ app/code/community/Fishpig/Bolt/Helper/Cache/Queue.php
@@ -24,30 +24,29 @@ class Fishpig_Bolt_Helper_Cache_Queue extends Mage_Core_Helper_Abstract
 	 */
 	public function flush()
 	{
-		$logFile = $this->_getLogFile();
+		$legacyLogFile = Mage::getBaseDir('var') . DS . 'bolt' . DS . 'flush-queue';
 		
-		if (!is_file($logFile)) {
-			return $this;
+		if (is_file($legacyLogFile)) {
+			@unlink($legacyLogFile);
 		}
 		
-		if (($queue = trim(@file_get_contents($logFile))) === '') {
-			return $this;
-		}
-		
-		$buffer = array();
-		
-		foreach(explode("\n", $queue) as $line) {
-			if (strpos($line, ':') !== false) {
-				list($id, $type) = explode(':', $line);
-				
-				if (!isset($buffer[$id])) {
-					$buffer[$id] = array();
-				}
-				
-				$buffer[$id][$type] = $type;
-			}
-		}
+		$this->ensureDbTableExists();
 
+		$table = $this->getTableName();
+		$write = Mage::getSingleton('core/resource')->getConnection('core_write');
+		$read  = Mage::getSingleton('core/resource')->getConnection('core_read');
+		
+		$logRecords = $read->fetchAll(
+			$read->select()
+				->from($table, array('entity_id', 'entity_type'))
+				->order('created_at DESC')
+				->limit(100)
+		);
+			
+		if (!$logRecords) {
+			return $this;
+		}
+		
 		$helper = Mage::helper('bolt/cache');
 		$models = array(
 			self::TYPE_CATEGORY => Mage::getModel('catalog/category'),
@@ -55,40 +54,40 @@ class Fishpig_Bolt_Helper_Cache_Queue extends Mage_Core_Helper_Abstract
 			self::TYPE_PRODUCT_CATEGORIES => Mage::getModel('catalog/product'),
 			self::TYPE_CMS_PAGE => Mage::getModel('cms/page'),
 		);
-
-		foreach($buffer as $id => $types) {
-			foreach($types as $type) {
-				if (!isset($models[$type])) {
-					continue;
-				}
-				
-				$model = $models[$type]->setId($id);
-
-				if ($type === self::TYPE_CATEGORY) {
-					$helper->refreshCategory($model);
-				}
-				else if ($type === self::TYPE_PRODUCT_CATEGORIES) {
-					$helper->refreshProduct($model, true);
-				}
-				else if ($type === self::TYPE_PRODUCT) {
-					$helper->refreshProduct($model, false);
-				}
-				else if ($type === self::TYPE_CMS_PAGE) {
-					$helper->refreshCmsPage($model);
-				}
-			}
-		}
 		
-		@unlink($logFile);
+		foreach($logRecords as $line) {
+			$id   = (int)$line['entity_id'];
+			$type = $line['entity_type'];
+			
+			if ($id === 0 || !isset($models[$type])) {
+				continue;
+			}
+			
+			$model = $models[$type]->setId($id);
+
+			if ($type === self::TYPE_CATEGORY) {
+				$helper->refreshCategory($model);
+			}
+			else if ($type === self::TYPE_PRODUCT_CATEGORIES) {
+				$helper->refreshProduct($model, true);
+			}
+			else if ($type === self::TYPE_PRODUCT) {
+				$helper->refreshProduct($model, false);
+			}
+			else if ($type === self::TYPE_CMS_PAGE) {
+				$helper->refreshCmsPage($model);
+			}
+			
+			$write->delete($table, $write->quoteInto('entity_id=' . (int)$id . ' AND entity_type=?', $type));
+		}
 
 		return $this;
 	}
 
-	
 	/**
 	 *
-	 * @param Mage_Catalog_Model_Product $product
-	 * @param bool $includeCategories = true
+	 * @param  Mage_Catalog_Model_Product $product
+	 * @param  bool $includeCategories = true
 	 * @return $this
 	 */
 	public function refreshProduct(Mage_Catalog_Model_Product $product, $includeCategories = true)
@@ -105,7 +104,7 @@ class Fishpig_Bolt_Helper_Cache_Queue extends Mage_Core_Helper_Abstract
 	
 	/**
 	 *
-	 * @param Mage_Catalog_Model_Category $category
+	 * @param  Mage_Catalog_Model_Category $category
 	 * @return $this
 	 */
 	public function refreshCategory(Mage_Catalog_Model_Category $category)
@@ -121,7 +120,7 @@ class Fishpig_Bolt_Helper_Cache_Queue extends Mage_Core_Helper_Abstract
 	 * Refresh a CMS page cache record
 	 * All sub pages are automatically refreshed
 	 *
-	 * @param Mage_Cms_Model_Page $page
+	 * @param  Mage_Cms_Model_Page $page
 	 * @return $this
 	 */
 	public function refreshCmsPage(Mage_Cms_Model_Page $page)
@@ -135,29 +134,70 @@ class Fishpig_Bolt_Helper_Cache_Queue extends Mage_Core_Helper_Abstract
 	
 	/*
 	 *
-	 * @param int $id
-	 * @param string $type
+	 * @param  int $id
+	 * @param  string $type
 	 * @return $this
 	 */
 	protected function _saveCacheFlushRequest($id, $type)
 	{
-		$logFile = $this->_getLogFile();
-		
-		if (!is_dir(dirname($logFile))) {
-			@mkdir(dirname($logFile));
+		$this->ensureDbTableExists();
+
+		try {
+			Mage::getSingleton('core/resource')->getConnection('core_write')
+				->insert(
+					$this->getTableName(),
+					array(
+						'entity_id'   => $id,
+						'entity_type' => $type,
+						'created_at'  => now(),
+					)
+				);
 		}
-		
-		@file_put_contents($logFile, $id . ':' . $type . "\n", FILE_APPEND | LOCK_EX);
+		catch (Exception $e) {
+			// Do nothing. Record already exists	
+		}
 		
 		return $this;
 	}
 	
 	/*
+	 * Ensure that the DB table exists
+	 *
+	 * @return $this
+	 */
+	protected function ensureDbTableExists()
+	{
+		$resource = Mage::getSingleton('core/resource');
+		$read     = $resource->getConnection('core_read');
+		$table    = $this->getTableName();
+		
+		try {
+			$read->fetchOne(
+				$read->select()->from($table, '*')->limit(1)
+			);
+
+		}
+		catch (Exception $e) {
+			$resource->getConnection('core_write')->query("
+				CREATE TABLE IF NOT EXISTS {$table} (
+					`entity_id` int(11) unsigned NOT NULL default 0,
+					`entity_type` varchar(32) NOT NULL default '',
+					`created_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+					PRIMARY KEY (`entity_id`, `entity_type`)
+				) ENGINE=MYISAM DEFAULT CHARSET=utf8 COMMENT='Queue for Bolt Auto Refresh';
+			");
+		}
+		
+		return $this;
+	}
+	
+	/*
+	 * Get the DB table
 	 *
 	 * @return string
 	 */
-	protected function _getLogFile()
+	protected function getTableName()
 	{
-		return Mage::getBaseDir('var') . DS . 'bolt' . DS . 'flush-queue';
+		return Mage::getSingleton('core/resource')->getTableName('bolt_flush_queue');
 	}
 }
